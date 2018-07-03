b1ee3febd25408233d35fd5412a998818d065b8a
efurdi27 <36687055+efurdi27@users.noreply.github.com>
2018-07-03 12:49:24 -0500

    Create PoliApp.java

---
+++ PoliApp.java
@@ -0,0 +1,12 @@
+public class PoliApp{
+
+public static void main(String [] args){
+  Gato gato=new Gato();
+  hazleHablar(gato);
+  Cucu cucu=new Cucu();
+  hazleHablar(cucu);
+  }
+  static void hazleHablar(Parlanchin suejto){
+  sujeto.habla();
+  }
+}
